11c79b4acb6f2226dff15cd501739743d1a9570f
Michael Pfister <michael@mp-development.de>
2014-07-05 12:20:40 +0200

    social share buttons

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
-/**
- * Created by mpfister on 04.07.14.
- */
+window.___gcfg = {
+    lang: 'en-US',
+    parsetags: 'onload'
+};
+
+window.onload = function () {
+    var blogDomain = "http://pfitzer.github.io/jgrunt";
+
+    !function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (!d.getElementById(id)) {
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "https://platform.twitter.com/widgets.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }
+    }(document, "script", "twitter-wjs");
+
+    (function() {
+        var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+        po.src = 'https://apis.google.com/js/plusone.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+    })();
+
+};
